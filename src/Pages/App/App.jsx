1237804b779db2d41df27dc9709bc19ee35de8f4
Antonio Jiménez <marshday5@gmail.com>
2024-01-14 00:24:06 -0600

    Adding react-router-dom to separate my app on differents routes to render the diffents components

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,4 +1,5 @@
-import './App.css';
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+
 import {Home} from "../Home";
 import {SingIn} from "../SingIn";
 import {MyAccount} from "../MyAccount";
@@ -6,18 +7,31 @@ import {MyOrder} from "../MyOrder";
 import {MyOrders} from "../MyOrders";
 import {NotFound} from "../NotFound";
 
-function App() {
+import './App.css';
 
+const AppRoutes = () => {
+  // se utiliza el hook useRoutes para definir las rutas de la aplicación.
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/sing-in", element: <SingIn /> },
+    { path: "/*", element: <NotFound /> }, //Estoy indicando que sea cualquier ruta que no este declarada
+  ]);
+
+  return routes;
+}
+
+
+const App = () => {
   return (
-    <>
-      <Home></Home>
-      <p className='bg-red-100'>Hola mundo xd</p>
-      <SingIn></SingIn>
-      <NotFound></NotFound>
-      <MyOrders></MyOrders>
-      <MyOrder></MyOrder>
-      <MyAccount></MyAccount>
-    </>
+    // el componente BrowserRouter para envolver las rutas de la aplicación. Esto proporciona la funcionalidad de enrutamiento para la aplicación.
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+
+    //Se incluye el componente AppRoutes dentro de BrowserRouter, indicando que las rutas definidas en AppRoutes se utilizarán para gestionar la navegación de la aplicación.
   )
 }
 
