14fbb3731d0b0f6ac940b4d6ccef205ceb1f40d4
Antonio Jiménez <marshday5@gmail.com>
2024-01-22 07:33:07 -0600

    fetch data to a api and using the data in form of props in the card component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 import { Layout } from '../../Components/Layout'
 import { Card } from '../../Components/Card'
 
 function Home() {
+  const [products, setProducts] = useState(null); //useState es un hook que se utiliza para agregar el estado a los componentes de función en React. Permite que un componente de función tenga un estado local, lo que significa que puede almacenar y modificar datos a lo largo del ciclo de vida del componente.
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, []); //se utiliza para realizar efectos secundarios en componentes de función. Estos efectos secundarios pueden incluir cosas como la manipulación del DOM, solicitudes de red, suscripciones a eventos y más. useEffect se ejecuta después de cada renderizado del componente y se utiliza para manejar lógica que no debería estar directamente en el flujo de renderizado principal del componente.
+
   return (
-    <Layout>
-        hola michi
-      <Card></Card>
+    <Layout >
+      <div className="grid place-items-center gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-screen-xl">
+        {
+          products?.map((product) => (
+            <Card 
+              key={product.id}
+              {...product}
+            />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
