1c48a2b7fc94eb31bedeedca8a10913af46d7cf9
Antonio Jiménez <marshday5@gmail.com>
2024-01-28 12:08:58 -0600

    implementing icons on card component

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
+import "boxicons";
 
 // eslint-disable-next-line react/prop-types
 function Card({category: {name}, images, price, title}) {
@@ -14,11 +15,12 @@ function Card({category: {name}, images, price, title}) {
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{name}</span>
         <img src={images[0]} alt={title} className='w-full h-full object-cover rounded-lg'/>
         <div 
-          className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-2'
+          className='absolute top-0 right-0 flex justify-center items-center bg-white w-10g h-10 rounded-full m-2 p-2'
           onClick={() => {
             context.setCount(context.count + 1);
           }}
-        >+
+        >
+          <box-icon type="solid" name="cart-add"></box-icon>
         </div>
       </figure>
       <p className='flex justify-between'>
