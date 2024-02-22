622fc5bb484c27f5c8710af546b98984e4c1f1a9
Antonio Jiménez <marshday5@gmail.com>
2024-02-21 23:51:36 -0600

    the view about the user is ready and i can redirect on the page

--- src/Pages/MyAccount/index.jsx
+++ src/Pages/MyAccount/index.jsx
@@ -1,9 +1,30 @@
+import { useContext } from 'react';
+import {useNavigate} from "react-router-dom";
 import {Layout} from '../../Components/Layout';
+import { ShoppingCartContext } from '../../Context';
+
 
 function MyAccount() {
+  const shoppingCartContext = useContext(ShoppingCartContext);
+  const {userApp, setUserApp, setSessionState, order} = shoppingCartContext;
+  
+  const navigate = useNavigate();
+
+  function closeSession ()
+    { 
+      setSessionState(false);
+      navigate("/");
+      setUserApp({});
+    }
   return (
     <Layout>
-      MyAccount
+      My account
+      <div className="h-52 w-52 bg-teal-700">
+        <p>Welcome{`${userApp.email}`}</p>
+
+        <button onClick={() => closeSession()}>Log out</button>
+      </div>
+
     </Layout>  
   )
 }
