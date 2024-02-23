8734d27097d9ab5fe4e550a20ec267ed110fd741
Antonio Jiménez <marshday5@gmail.com>
2024-02-22 23:43:45 -0600

    protecting routes

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,4 +1,5 @@
-import { useRoutes, BrowserRouter } from 'react-router-dom';
+import { useContext } from 'react';
+import { useRoutes, BrowserRouter, Navigate } from 'react-router-dom';
 
 import {Home} from "../Home";
 import {SingIn} from "../SingIn";
@@ -7,24 +8,26 @@ import {MyOrder} from "../MyOrder";
 import {MyOrders} from "../MyOrders";
 import {NotFound} from "../NotFound";
 import {Navbar} from "../../Components/Navbar";
-import {ShoppingCartProvider} from "../../Context/index";
-import './App.css';
 import { CheckoutSideMenu } from '../../Components/CheckoutSideMenu';
+import { ShoppingCartContext, ShoppingCartProvider } from "../../Context/index";
+import './App.css';
 
-const AppRoutes = () => {
+
+
+const AppRoutes = ({sessionState}) => {
   // se utiliza el hook useRoutes para definir las rutas de la aplicación.
   const routes = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "/clothes", element: <Home /> },
-    { path: "/electronics", element: <Home /> },
-    { path: "/furnitures", element: <Home /> },
-    { path: "/toys", element: <Home /> },
-    { path: "/gallos", element: <Home /> },
-    { path: "/my-order", element: <MyOrder /> },
-    { path: "/my-orders", element: <MyOrders /> },
-    { path: "/my-orders/last", element: <MyOrder /> },
+    { path: "/", element: sessionState ? <Home /> :  <Navigate to="/sing-in" /> },
+    { path: "/clothes", element: sessionState ? <Home /> :  <Navigate to="/sing-in" />},
+    { path: "/electronics", element: sessionState ? <Home /> :  <Navigate to="/sing-in" />},
+    { path: "/furnitures", element: sessionState ? <Home /> :  <Navigate to="/sing-in" />},
+    { path: "/toys", element: sessionState ? <Home /> :  <Navigate to="/sing-in" />},
+    { path: "/gallos", element: sessionState ? <Home /> :  <Navigate to="/sing-in" />},
+    { path: "/my-order", element: sessionState ? <MyOrder /> : <Navigate to="/sing-in" />},
+    { path: "/my-orders", element: sessionState ? <MyOrders /> : <Navigate to="sing-in" />},
+    { path: "/my-orders/last", element: sessionState ? <MyOrder /> : <Navigate to="sing-in" />},
     { path: "/my-orders/:id", element: <MyOrder /> },
-    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-account", element: sessionState ? <MyAccount /> : <Navigate to="sing-in"/>},
     { path: "/:url", element: <SingIn /> },
     { path: "/*", element: <NotFound /> }, //Estoy indicando que sea cualquier ruta que no este declarada
   ]);
@@ -34,15 +37,19 @@ const AppRoutes = () => {
 
 
 const App = () => {
+  
+
+  console.log("ShoppingCartContext:", useContext(ShoppingCartContext));
+
   return (
     // el componente BrowserRouter para envolver las rutas de la aplicación. Esto proporciona la funcionalidad de enrutamiento para la aplicación.
     <ShoppingCartProvider>
       <BrowserRouter>
-        <CheckoutSideMenu/>
+        <CheckoutSideMenu />
         <AppRoutes />
         <Navbar />
       </BrowserRouter>
-    </ShoppingCartProvider>  
+    </ShoppingCartProvider>
     //Se incluye el componente AppRoutes dentro de BrowserRouter, indicando que las rutas definidas en AppRoutes se utilizarán para gestionar la navegación de la aplicación.
   )
 }
--- src/Pages/MyAccount/index.jsx
+++ src/Pages/MyAccount/index.jsx
@@ -25,8 +25,27 @@ function MyAccount() {
 
         <button onClick={() => closeSession()}>Log out</button>
       </div>
-
+      <div className="flex justify-between items-center mb-3 border border-emerald-800 rounded-lg mt-6">
+            <div className="flex items-center gap-2">
+                <p className="text-2xl flex justify-between items-center p-4 font-normal">
+                    <span>
+                        <span className="text-xl flex items-center">
+                            <box-icon type='solid' name='calendar' size="sm"></box-icon>
+                        </span> 
+                        <span className="text-xl flex items-center">
+                            <box-icon name='cart' size="sm"></box-icon>
+                            Articles:
+                        </span>
+                    </span>
+                    <span className="font-bold ml-8">$</span>
+                </p>
+            </div>
+        </div>
     </Layout>  
+
+
+
+
   )
 }
 
